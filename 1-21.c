e82e13965b55c29d68d0668a6e118d770e90b196
ngfuwai <tigerw119@gmail.com>
2018-05-15 13:01:52 -0400

    Create 1-21.c

---
+++ 1-21.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define NUMCOL 3
+#define MAXLINE 1000
+
+int entab(char str[]);
+
+int main()
+{
+  char str[MAXLINE];
+  int x = entab(str);
+  
+  if(x < MAXLINE - 1 && x > 0)
+    printf("%s\n", str);
+
+  else
+    printf("invalid input\n"); // note that newline is a character
+
+  return 0;
+}
+
+int entab(char str[])
+{
+  int i = 0, x, ctr = 0;
+  char c;
+
+  while(i < MAXLINE - 1 && (c = getchar()) != EOF)
+    {
+      if(c == ' ')
+	  ++ctr;
+
+      if(ctr == 3)
+	{
+	  i -= 2;
+	  str[i] = '\t';
+	  ctr = 0;
+	}
+
+      else
+	str[i] = c;
+
+      ++i;
+    }
+
+  str[i] = '\0';
+
+  return i;
+}
